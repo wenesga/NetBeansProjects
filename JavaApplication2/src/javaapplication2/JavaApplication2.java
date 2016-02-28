7806a248c53459a01f4c0a98a098cc4a24cce9f6
WENES GOMES AQUINO <wenesga@gmail.com>
2016-02-28 12:28:41 -0300

    no message

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -1,12 +1,6 @@
-/*
- * To change tggghis license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the tempfffffffffffflate in the editor.
- */
 package javaapplication2;
 
 /**
- *
  * @author WENES GOMES AQUINO
  */
 public class JavaApplication2 {
