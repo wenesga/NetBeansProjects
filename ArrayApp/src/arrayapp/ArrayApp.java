f379453a3ea4cd5f52c82ecf6370a119ef64e2a3
WENES GOMES AQUINO <wenesga@gmail.com>
2016-02-29 23:47:36 -0300

    Array

--- ArrayApp/src/arrayapp/ArrayApp.java
+++ ArrayApp/src/arrayapp/ArrayApp.java
@@ -1,11 +1,9 @@
 package arrayapp;
-
 import java.util.Random;
 import java.util.Scanner;
 
 /**
  * ArrayApp [TIPO] Descrição:
- *
  * @author WENES GOMES AQUINO <wenesga@gmail.com>
  * @date 29/02/2016
  */
@@ -17,19 +15,32 @@ public class ArrayApp {
 
             Scanner ler = new Scanner(System.in);
 
-            System.out.println("Entre com sua posições de array");
+            System.out.println("Entre com a primeira posiçoes: ");
             int posicao = ler.nextInt();
-            
-            int [] A = new int [posicao];
-            int [] B = new int [posicao];
-            int [] C = new int [posicao];
-            
-            Random r = new Random ();
 
-        }
-        
-        catch {
+            int[] A = new int[posicao];
+            int[] B = new int[posicao];
+            int[] C = new int[posicao];
+
+            Random r = new Random();
             
+            for (int i = 0; i < posicao; i++) {
+                A [i] = r.nextInt(50);
+                B [i] = r.nextInt(50);
+            }
+            
+            for (int j = 0; j < posicao; j++) {
+                C [j] = A[j] + B[j];
+            }
+            
+            for (int k = 0; k < posicao; k++) {
+                System.out.printf("%5d %8d\n%d\n", A[k], B[k], C[k]);
+            }
+
+        } 
+        
+        catch (Exception ex) {
+            System.out.printf("PRIMEIRA EXCESAO.\n");
         }
 
     }
