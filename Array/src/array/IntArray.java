33757adcc46ae26e8574e4216f5d95189542a098
WENES GOMES AQUINO <wenesga@gmail.com>
2016-02-29 23:48:48 -0300

    no message

--- Array/src/array/IntArray.java
+++ Array/src/array/IntArray.java
@@ -1,10 +1,8 @@
 package array;
-
 import java.util.InputMismatchException;
-import java.util.Scanner;
+
 /**
  * Array [TIPO] Descrição: Descrição:
- *
  * @author WENES GOMES AQUINO <wenesga@gmail.com>
  * @date 29/02/2016
  */
@@ -14,27 +12,24 @@ public class IntArray {
 
         try {
 
-            final int ARRAY_LENGTH = 10; // declara constante      
-            int[] array = new int[ARRAY_LENGTH]; // cria o array
+            final int ARRAY_LENGTH = 5;       
+            
+            int[] array = new int[ARRAY_LENGTH]; 
 
-            // calcula o valor de cada elemento do array
+
             for (int counter = 0; counter < array.length; counter++) {
-                array[counter] = 2 + 2 * counter;
+                array[counter] = 2 + 2 / counter;
             }
 
-            System.out.printf("%s%8s\n", "Index", "Value"); // títulos de coluna
+            System.out.printf("%s%8s\n", "Indice", "Valor"); 
 
-            // gera a saída do valor de cada elemento do array
             for (int counter = 0; counter < array.length; counter++) {
                 System.out.printf("%5d%8d\n", counter, array[counter]);
-
             }
-
-        } catch (InputMismatchException inputMismatchException) {
-            System.err.printf("\nException: %s\n", inputMismatchException);
-            scanner.nextLine(); // descarta entrada para o usuário poder tentar de novo          
-            System.out.println("You must enter integers. Please try again.\n");
+        } 
+        
+        catch (Exception ex) {
+            System.out.println("PRIMEIRA EXCESAO.\n");
         }
     }
-
 }
--- TratamentoEx/src/tratamentoex/TramentoEx.java
+++ TratamentoEx/src/tratamentoex/TramentoEx.java
@@ -1,17 +1,17 @@
 package tratamentoex;
-// wenes
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TramentoEx {
 
-    // demonstra o lançamento de uma exceção quando ocorre uma divisão por zero
     public static int quotiente(int numerador, int denominador) {
-        return numerador / denominador;  // possível divisão por zero
+        return numerador / denominador; 
     }
 
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
+        
         boolean continueLoop = true; // determina se mais entradas são necessárias
 
         do {
@@ -23,19 +23,25 @@ public class TramentoEx {
                 int denominador = scanner.nextInt();
 
                 int resultado = quotiente(numerador, denominador);
+                
                 System.out.printf("\nResultado: %d / %d = %d\n", numerador, denominador, resultado);
+                
                 continueLoop = false; // entrada bem­sucedida; fim do loop                  
             } 
             
             catch (InputMismatchException inputMismatchException) {
+                
                 System.err.printf("\nExceção: %s\n", inputMismatchException);
                 scanner.nextLine(); // descarta entrada para o usuário poder tentar de novo
                 System.out.println("Você deve entrar com um numero interio. Tente novamente.\n");
+            } 
             
-            } catch (ArithmeticException arithmeticException) {
+            catch (ArithmeticException arithmeticException) {
+                
                 System.err.printf("\nExceção: %s\n", arithmeticException);
                 System.out.println("O denominador tem que ser diferente de zero. Tente novamente.\n");
             }
+            
         } while (continueLoop);
     }
 }
